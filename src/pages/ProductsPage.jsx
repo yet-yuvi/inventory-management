5eb4ac48c1086d18e4bce2b762897a291c8aa4e1
Jubayar Ahamad <oscyanogen@gmail.com>
2024-03-07 02:55:16 +0600

    Commit-8 : Add Link and useNavigate button to change route as ProductForm, and then add back button to route back to products

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
-import { Products } from '../components'
+import React from "react";
+import { Link, Outlet, useNavigate } from "react-router-dom";
+import { Box, Button, Stack } from "../ui";
 
 export const ProductsPage = () => {
+  const navigate = useNavigate();
+  const routeToProductForm = () => navigate("/products/add");
+
   return (
-    <Products />
-  )
-}
+    <Stack>
+      
+      <Box justifyContent="flex-end" px={2} py={2}>
+        <Link to="/products/add">
+          <Button 
+          variant="contained" 
+          size="small">
+            Add Product
+          </Button>
+        </Link>
+      </Box>
+
+      <Box justifyContent="flex-end" px={2} py={2}>
+        <Button variant="contained" size="small" onClick={routeToProductForm}>
+          Add Product
+        </Button>
+      </Box>
+
+      <Outlet />
+
+    </Stack>
+  );
+};
